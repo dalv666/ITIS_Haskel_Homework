f6808081739eee0207b89f79224e49ce0b6e575e
DjnagoMaster <dalv6666@gmail.com>
2015-04-29 22:47:41 +0300

    Revisal

--- src/Semestr02/TypedLambdaCheker.hs
+++ src/Semestr02/TypedLambdaCheker.hs
@@ -1,37 +1,43 @@
 module TypedLambdaCheker where
 
---de Bruijn indices variables representation
-type Name = Int
+type Name = Char
 
---Not need to check
-data InferTerm = Check CheckTerm Type | Var Int | Par Int | App InferTerm CheckTerm 
---annotation/bound variables/parameters/application
+data Term =  Var Name 
+			| Abstr Name Type Term 
+			| App Term Term 
+			| Par ParType
 
---Need to check
-data CheckTerm = Infer InferTerm | Lam CheckTerm 
+data Type =  TypeInt 
+			| TypeBool 
+			| TypeExsp 
+			| Fun Type Type deriving (Show, Eq, Ord)
 
-data Type = TPar Int | Fun Type Type 
--- sigma, tau, etc. -- sigma -> tau
+data ParType = ParInt Int 
+			| ParBool Bool  deriving (Show, Eq, Ord)
 
-type Context = [(Name,Type)]
---bind name and type
+type Context = [(Name, Type)]
 
---Substitution function
-subst :: Int -> InferTerm -> InferTerm -> InferTerm
-subst name iTerm (Par name2) = Par name2
-subst name1 term1 (App term2 term3) = App (subst name1 term1 term2) (Infer (subst name1 term1 (Check term3 (TPar name1))))
-subst name1 term1 (Var name2)
-							| name1 == name2     = term1
-							| otherwise          = Var name2
 
---Checking function
-checkType :: Int -> Context -> Type -> CheckTerm -> Maybe()
-checkType name iTerm (Fun type1 type2) (Lam cTerm) = do checkType (name + 1) ((name,type1 ):iTerm) type2 (Infer (subst 0 (Par name) (Check cTerm (Fun type1 type2))))
+look :: Context -> Char -> Type
+look [] name = TypeExsp
+look ((name,type1):context) name2 | name == name2    = type1
+                                  | otherwise		 = look context name2
 
---Deducing function
-inferType :: Int -> Context -> InferTerm -> Maybe Type
-inferType name iTerm (Par name2) = lookup name2 iTerm
-inferType name iTerm (App iTerm2 cTerm) = do
-						  Fun type1 type2 <- inferType name iTerm iTerm2
-						  checkType name iTerm type1 cTerm
-						  return type2
+checkOnFun :: Type -> Type -> Type
+checkOnFun (Fun term1 term2) term3 = if (term1 == term3) 
+									then term2
+									else TypeExsp
+checkOnFun _ _ = TypeExsp
+
+checkTerm :: Context -> Term -> Type
+checkTerm _ (Par (ParInt a)) = TypeInt
+checkTerm _ (Par (ParBool a)) = TypeBool
+checkTerm context (Var x) = look context x
+checkTerm context (App term1 term2) = if ((checkTerm context term1) == TypeExsp || (checkTerm context term2) == TypeExsp) 
+                                    then TypeExsp else checkOnFun (checkTerm context term1) (checkTerm context term2)
+checkTerm context (Abstr name type1 term) =  if ((checkTerm ((name,type1):context) term) /= TypeExsp) 
+									then Fun type1 (checkTerm ((name,type1):context) term)
+									else TypeExsp
+
+check :: Type -> Bool
+check type1 = not (type1 == TypeExsp) 
